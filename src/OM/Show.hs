19db40b54ac89babe37562d0063152785d5f2c39
Rick Owens <rick@owensmurray.com>
2022-06-12 10:07:32 -0500

    feat: Add ShowJ.

--- src/OM/Show.hs
+++ src/OM/Show.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 {- | Utilities for showing string-like things. -}
 module OM.Show (
   showt,
   showj,
+  ShowJ(..),
 ) where
 
 
@@ -20,5 +23,13 @@ showt = fromString . show
 {- | Show the JSON representation as any kind of string-like thing. -}
 showj :: (ToJSON a, IsString b) => a -> b
 showj = fromString . TL.unpack . TLE.decodeUtf8 . encode
+
+
+{- | Wrapper whose 'Show' instance outputs JSON. -}
+newtype ShowJ a = ShowJ a
+  deriving stock (Eq, Ord)
+  deriving newtype (ToJSON)
+instance (ToJSON a) => Show (ShowJ a) where
+  show = showj
 
 
